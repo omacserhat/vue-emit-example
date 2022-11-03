2de82ebe4c250b980f1ca4d9d849a5ffdbe675dc
Serhat OMAÇ <omacserhatt@gmail.com>
2022-11-03 16:18:52 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+  <div id="app">
+  <alt-component @toplamasonucu="gelen($event)"/>
+  Üst Componente Gelen Değer{{hesaplama}}
+  </div>
+  </div>
+  </template>
+
+
+  <script>
+  import AltComponent from "./components/AltComponent.vue";
+  export default {
+  name: "App",
+  data() {
+  return {
+  hesaplama: 0
+  };
+  },
+  methods: {
+  gelen(sonuc) {
+  this.hesaplama = sonuc;
+  }
+  },
+  components: {
+  AltComponent
+  }
+  };
+  </script>
+  
---
+++ src/components/AltComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+    <p>Alt Component</p>
+    <p>Sayı 1</p>
+    <input type="text" v-model="sayi1">
+    <p>Sayı 2</p>
+    <input type="text" v-model="sayi2">
+    <button @click="topla()">Üst'e Gönder</button>
+    </div>
+    </template>
+    <script>
+    export default {
+    name: "AltComponent",
+    data() {
+    return {
+    sayi1: 0,
+    sayi2: 0,
+    };
+    },
+    methods: {
+    topla() {
+    let sonuc = +this.sayi1 + +this.sayi2;
+    this.$emit("toplamasonucu", sonuc);
+    }
+    }
+    };
+    </script>
